89bdfdff707ce7d099a786ca639b68e93330712a
Kenji <kenji0x02@gmail.com>
2014-11-09 02:15:09 +0900

    change checksum from sha1 to sha256

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,7 +25,8 @@
 #
 
 default['mecab']['version'] = '0.996'
-default['mecab']['checksum'] = '15baca0983a61c1a49cffd4a919463a0a39ef127'
+# SHA256
+default['mecab']['checksum'] = 'e073325783135b72e666145c781bb48fada583d5224fb2490fb6c1403ba69c59'
 
 default['mecab']['ipadic']['version'] = '2.7.0-20070801'
 default['mecab']['ipadic']['checksum'] = '0d9d021853ba4bb4adfa782ea450e55bfe1a229b'
